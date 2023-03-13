91dc69bafb5eb219e1745e55e79914de53b2c940
Bobby <125319140+InterestedInCyber@users.noreply.github.com>
2023-03-13 22:50:03 +0000

    Add files via upload

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace RockPaperScissors
+{
+    sealed class Program
+    {
+        static void Main(string[] args)
+        {
+            bool playing = true;
+
+            Console.WriteLine("Welcome to Rock Paper Scissors");
+
+            while (playing)
+            {
+                string[] RSP = { "Rock", "Paper", "Scissors" };
+                Random Pick = new Random();
+                int PickIndex = Pick.Next(RSP.Length);
+                string RandomPick = RSP[PickIndex];
+
+                Console.Write("Pick Your Poison (Rock, Paper or Scissors)? ");
+                string? Option = Console.ReadLine();
+
+                if (Option == RandomPick)
+                {
+                    Console.WriteLine($"You see both of you picking {Option} is BORING! Pick Something Different");
+                }
+                else if (Option == "Rock" && RandomPick == "Scissors" || Option == "Paper" && RandomPick == "Rock" || Option == "Scissors" && RandomPick == "Paper")
+                {
+                    Console.WriteLine($"{Option} does in fact beat {RandomPick}. Congratulations!");
+                }
+                else
+                {
+                    Console.WriteLine($"You know that {RandomPick} beats {Option} right?");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
